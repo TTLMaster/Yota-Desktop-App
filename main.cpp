d97fb3ec2dabe268fcf194cf627ce5e4f580653e
kolyanok <kolyanok@users.noreply.github.com>
2017-07-01 23:48:16 +0300

    Revert "And..."
    This reverts commit bc43010eab9164997ea4bd84886c7c1c8ed7a881.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #define P_VERSION "1.0.0"
 int main(int argc, char *argv[])
 {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     Yota w;
     w.show();
