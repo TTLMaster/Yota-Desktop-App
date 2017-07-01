bc43010eab9164997ea4bd84886c7c1c8ed7a881
kolyanok <kolyanok@users.noreply.github.com>
2017-07-01 23:46:23 +0300

    And...

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #define P_VERSION "1.0.0"
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     Yota w;
     w.show();
